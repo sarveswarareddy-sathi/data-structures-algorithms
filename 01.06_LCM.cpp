69d92a04a92f03d5bb5eb53b7ccd6572599da904
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-26 14:51:16 +0000

    Data Structures & Algorithms

---
+++ 01.06_LCM.cpp
@@ -0,0 +1,15 @@
+// least common divisible by both
+
+int naive_lcm(int a, int b) {
+    int res = max(a,b);
+    while(true) {
+        if(res % a == 0 && res % b == 0) return res;
+        res++;
+    }
+    return res;
+}
+
+//time complexity - O(a*b - max(a,b))
+
+// efficient a*b = gcd(a,b) * lcm(a,b)
+// gcd comp - log (min(a,b))
