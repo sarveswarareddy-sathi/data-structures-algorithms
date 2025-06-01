8c43e96a5bccb7a33a25d9a83ecdc5971b1439f0
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-01 07:12:36 +0000

    Data Structures & Algorithms

--- 01.05_GCD.cpp
+++ 01.05_GCD.cpp
@@ -12,8 +12,45 @@ int naive_gcd(int a, int b) {
     return res;
 }
 
+/*
+Euclidean Algorithm : 
+--> Let 'b' be smaller than 'a', then gcd(a,b) = gcd(a-b, b)
+--> Lets say, 'd' = gcd(a,b) this means 'd' divides both 'a' and 'b'
+--> The 'd' must also divide a-b which is common divisor of 'a-b' and 'b'
+
+Let’s say, a = 15, b = 6 :
+Step 1: a > b → a = 15 - 6 = 9
+Step 2: a > b → a = 9 - 6 = 3
+Step 3: b > a → b = 6 - 3 = 3
+Now a == b == 3 → return 3
+
+
+Let’s say, a = 9, b = 4 :
+a > b → a = 9 - 4 = 5
+a > b → a = 5 - 4 = 1
+b > a → b = 4 - 1 = 3
+b > a → b = 3 - 1 = 2
+b > a → b = 2 - 1 = 1
+Now a == b == 1 → return 1
+
+
+*/
+
+//Euclidean Algo - Approach 1
+int gcd_aproch1(int a, int b) {
+    while (a!=b) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
+    return a;
+}
+
 // More Efficient Approach - Euclidean Algorithm
 // Using mod
+// gcd(a,b) = gcd(b,a % b)
 int eff_gcd(int a, int b) {
     if (b == 0) {
         return a;
