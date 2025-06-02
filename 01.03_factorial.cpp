80f9797b794c8bded4e7ce1c761f4d0bbc53c40f
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-02 10:30:14 +0800

    Data Structures & Algorithms

--- 01.03_factorial.cpp
+++ 01.03_factorial.cpp
@@ -1,5 +1,12 @@
 /*
+---> Factorial Recursive - Multiplication of all numbers less than or equal to given number
+---> n! = n*(n-1)!, n = 1 for n==0 or n==1
+---> Time Complexity - O(n)
+---> Auxiliary Space - O(n)
 
+---> Factorial Iterative - res* = i using for loop()
+---> Time Complexity - O(n)
+---> Auxiliary Space - O(1)
 */
 
 #include <stdio.h>
