e09837ef6cfd1edec6b2cd8d42c42f5242a42c88
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-02 13:20:20 +0800

    Data Structures & Algorithms

--- 01.06_LCM.cpp
+++ 01.06_LCM.cpp
@@ -1,3 +1,10 @@
+/*
+---> LCM of two members is the smallest number which can be divided by both numbers
+---> LCM is smallest 'x' : x%a == 0 (I.E., X is a multiple of 'a')
+--->                 'x' : x%b == 0 (I.E., X is a multiple of 'b')
+---> Smallest common multiples
+---> Efficient Apporach 1 : LCM can be atleast as large as larger of the two - max(a,b) and largest possible a*b. Iterate over max(a,b) to a*b
+*/
 // least common divisible by both
 // For co-prime - a*b
 
@@ -13,4 +20,5 @@ int naive_lcm(int a, int b) {
 //time complexity - O(a*b - max(a,b))
 
 // efficient a*b = gcd(a,b) * lcm(a,b)
+
 // gcd comp - log (min(a,b))
