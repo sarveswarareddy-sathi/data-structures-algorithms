094a3726978a924fb91d99e7ab1f624050dac97d
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-04 11:36:59 +0800

    Data Structures & Algorithms

--- 01.10_Eratosthenes.cpp
+++ 01.10_Eratosthenes.cpp
@@ -2,6 +2,12 @@
  --> Prime numbers less than given number
  Sieve of Eratosthenes : works well < 10 million or so
  naive - O(n*sqrt(n))
+ sieve - O(n*log(log(n))) & O(n)
+
+ Sieve of Eratosthenes : 
+ 1. Create an array of true values - representing potential primes 2 to n
+ 2. Start with smallest prime '2' - mark all multiples of '2' not prime
+ 3. repeat it for '3', '5' and continue it till sqrt(n)
 */
 
 void printPrime_naive(int n) {
