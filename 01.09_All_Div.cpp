5cbd9f6d096fece8aefea0604a5fca28f87e9959
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-04 11:06:50 +0800

    Data Structures & Algorithms

--- 01.09_All_Div.cpp
+++ 01.09_All_Div.cpp
@@ -1,5 +1,14 @@
 /*
+Given a natural number 'n', print all distinct divisors of it.
+Approach 1 : Iterate all numbers from '1' to 'n'/'sqrt(n)', checking if that number divides 'n'. need print both 'i' and 'n/i'
 Time complexity - O(n)
+Auxiliary Space - O(1)
+
+Sorted Order Approach 2 :
+1. '1' to 'sqrt(n)' : print 'i'
+2. If exactly equals to sqrt(n) 
+3. Reverse loop - every divisor 'i' has complemantary divisor 'n/i' except i == sqrt(n)
+Time complexity - O(SQRT(n))
 Auxiliary Space - O(1)
 */
 void printDiv_naive(int n) {
