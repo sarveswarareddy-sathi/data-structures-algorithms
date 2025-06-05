89ba7c288aef3838101877d8d0782406beab6fd8
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-05 11:23:13 +0000

    Data Structures & Algorithms

--- 01.12.Mod.cpp
+++ 01.12.Mod.cpp
@@ -1,1 +1,12 @@
+/*
+Quotient Remainder Theorem : For any pair of integers 'a' and 'b', there exist two unique integers 'q' and 'r' such that
+   ---> a = b*q + r where 0 <= r < b;
 
+Rules :
+   ---> (a+b)modm = ((a mod m) + (b mod m)) mod m
+   ---> (a*b)modm = ((amodm)(bmodm))modm
+   ---> (a/b)modm = (a * (inv of b if exists)) mod m
+
+---> Modular Inverse : The modular inverse of a mod m exists only if a and m are relatively prime i.e gcd(a,m) = 1.
+    Hence, for finding 
+*/
