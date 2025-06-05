ea8833669f6344174d6d42b66726e1305423f6f9
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-06-05 15:09:45 +0000

    Data Structures & Algorithms

---
+++ 01.15.Celsius_To_Fahrenheit.cpp
@@ -0,0 +1,11 @@
+//Expected Time Complexity: O(1)
+//Expected Auxiliary Space : O(1)
+class Solution {
+  public:
+
+    // You need to complete this function
+    double cToF(int C) {
+        // Your code here
+        return double(C*(9.0/5.0) + 32);
+    }
+};
